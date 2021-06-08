ca6d14e8a92ed861150643b193557d8c50acca15
Brayan <brayan.11fera@gmail.com>
2021-06-08 16:40:00 -0300

    alteração na criação da tabela gestor

--- src/main/resources/db/migration/V0001__createTableGestores.sql
+++ src/main/resources/db/migration/V0001__createTableGestores.sql
@@ -1,9 +1,9 @@
 CREATE TABLE gestor (
-  idGestor bigint NOT NULL,
+  id_gestor bigint NOT NULL,
   senha varchar(100) NOT NULL,
-  idSecao int NOT NULL,
-  nomeSecao varchar(100) NOT NULL,
-  nomeGestor varchar(100) NOT NULL,
+  id_secao int NOT NULL,
+  nome_secao varchar(100) NOT NULL,
+  nome_gestor varchar(100) NOT NULL,
   email varchar(100) NOT NULL,
-  PRIMARY KEY (idGestor)
+  PRIMARY KEY (id_gestor)
 )
