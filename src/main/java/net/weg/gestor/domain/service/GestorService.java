d457a2de4c7ae52bd43bbb67a5bf14a77aeacfe7
Brayan <brayan.11fera@gmail.com>
2021-06-08 16:39:46 -0300

    Adicionado os métodos de excluir, editar, buscar, e listar

--- src/main/java/net/weg/gestor/domain/service/GestorService.java
+++ src/main/java/net/weg/gestor/domain/service/GestorService.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 import net.weg.gestor.domain.exception.NegocioException;
 import net.weg.gestor.domain.model.Gestor;
 import net.weg.gestor.domain.repository.GestorRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Service
@@ -15,15 +18,47 @@ public class GestorService {
 
     @Transactional
     public Gestor cadastrar(Gestor gestor) {
+        boolean emailValidation = gestorRepository.findByEmail(gestor.getEmail()).isPresent();
+        if (emailValidation) {
+            throw new NegocioException("Já existe um usuario com esse email");
+        }
 
-//        boolean emailValidation = gestorRepository.findByEmail(gestor.getEmail()).isPresent();
-//
-//        if (emailValidation) {
-//            throw new NegocioException("Já existe um usuario com esse email");
-//
-//        }
+        boolean idGestorValidation = gestorRepository.findByidGestor(gestor.getIdGestor()).isPresent();
+        if (idGestorValidation) {
+            throw new NegocioException("Já existe um gestor com esse ID");
+        }
 
         return gestorRepository.save(gestor);
+    }
+
+    public ResponseEntity<Gestor> excluir(Long gestorId) {
+        if(!gestorRepository.existsById(gestorId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        gestorRepository.deleteById(gestorId);
+        return ResponseEntity.noContent().build();
 
     }
+
+    public ResponseEntity<Gestor> editar(Long gestorId, Gestor gestor) {
+        if(!gestorRepository.existsById(gestorId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        gestor.setIdGestor(gestorId);
+        gestor = gestorRepository.save(gestor);
+        return ResponseEntity.ok(gestor);
+    }
+
+    public ResponseEntity<Gestor> buscar(Long gestorId) {
+        return gestorRepository.findById(gestorId).map(gestor -> ResponseEntity.ok(gestor))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    public List<Gestor> listartodos() {
+        return gestorRepository.findAll();
+    }
+
+
 }
