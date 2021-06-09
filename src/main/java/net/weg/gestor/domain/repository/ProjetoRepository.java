e1308d5dfad60912b52fb7ad56b451865e510145
Brendon <brendon3007gabriel@gmail.com>
2021-06-09 16:29:15 -0300

    Repositorys

--- src/main/java/net/weg/gestor/domain/repository/ProjetoRepository.java
+++ src/main/java/net/weg/gestor/domain/repository/ProjetoRepository.java
@@ -1,2 +1,12 @@
-package net.weg.gestor.domain.repository;public interface ProjetoRepository {
+package net.weg.gestor.domain.repository;
+
+import net.weg.gestor.domain.model.Projeto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
+
+
+
 }
--- src/main/java/net/weg/gestor/domain/repository/SecaoRepository.java
+++ src/main/java/net/weg/gestor/domain/repository/SecaoRepository.java
@@ -1,2 +1,15 @@
-package net.weg.gestor.domain.repository;public interface SecaoRepository {
+package net.weg.gestor.domain.repository;
+
+import ch.qos.logback.core.pattern.parser.OptionTokenizer;
+import net.weg.gestor.domain.model.Secao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SecaoRepository extends JpaRepository<Secao, Long> {
+
+    Optional<Secao> findById(long secaoId);
+
 }
