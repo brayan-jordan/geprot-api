f3403068839968e529685ce2bea627d20ba4b083
Brendon <brendon3007gabriel@gmail.com>
2021-06-10 11:36:53 -0300

    Projeto service

--- src/main/java/net/weg/gestor/domain/service/ProjetoService.java
+++ src/main/java/net/weg/gestor/domain/service/ProjetoService.java
@@ -1,2 +1,40 @@
-package net.weg.gestor.domain.service;public class ProjetoService {
+package net.weg.gestor.domain.service;
+
+
+import lombok.AllArgsConstructor;
+import net.weg.gestor.domain.exception.NegocioException;
+import net.weg.gestor.domain.model.Projeto;
+import net.weg.gestor.domain.model.StatusProjeto;
+import net.weg.gestor.domain.repository.GestorRepository;
+import net.weg.gestor.domain.repository.ProjetoRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ProjetoService {
+    private ProjetoRepository projetoRepository;
+    private GestorRepository gestorRepository;
+
+    public List<Projeto> listartodos() {
+        return projetoRepository.findAll();
+    }
+
+    public Projeto cadastrar(Projeto projeto){
+        boolean gestorVerification = gestorRepository.findById(projeto.getGestorid()).isPresent();
+        if(!gestorVerification){
+            throw new NegocioException("Não existe um gestor com esse ID ");
+        }
+
+        projeto.setHorastrabalhadas(0);
+        projeto.setDatainicio(LocalDateTime.now());
+        projeto.setHorastrabalhadas(0);
+        projeto.setValorutilizado(0);
+        projeto.setValorrestante(projeto.getValorprojeto() - projeto.getValorutilizado());
+        projeto.setStatusprojeto(StatusProjeto.EM_ANDAMENTO);
+        return projetoRepository.save(projeto);
+
+    }
 }
