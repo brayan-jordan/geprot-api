baca44b5dc9afa1fcad742e64568c50b94a448c2
Brendon <brendon3007gabriel@gmail.com>
2021-06-10 11:36:34 -0300

    Controlador das rotas de projeto

--- src/main/java/net/weg/gestor/controller/ProjetoController.java
+++ src/main/java/net/weg/gestor/controller/ProjetoController.java
@@ -1,2 +1,30 @@
-package net.weg.gestor.controller;public class ProjetoController {
+package net.weg.gestor.controller;
+
+import lombok.AllArgsConstructor;
+import net.weg.gestor.domain.model.Projeto;
+import net.weg.gestor.domain.service.ProjetoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/projetos")
+@AllArgsConstructor
+public class ProjetoController {
+
+    private ProjetoService projetoService;
+
+    @GetMapping("/listar")
+    public List<Projeto> listarTodosProjetos(){
+        return projetoService.listartodos();
+    }
+
+    @PostMapping("/cadastrar")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Projeto cadastraProjeto(@Valid @RequestBody Projeto projeto){
+        return projetoService.cadastrar(projeto);
+    }
+
 }
