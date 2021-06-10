e7de8c6ac8a65992b01da37204b6849a9111b52b
Brendon <brendon3007gabriel@gmail.com>
2021-06-10 11:36:43 -0300

    Projeto repository

--- src/main/java/net/weg/gestor/domain/repository/ProjetoRepository.java
+++ src/main/java/net/weg/gestor/domain/repository/ProjetoRepository.java
@@ -2,11 +2,19 @@ package net.weg.gestor.domain.repository;
 
 import net.weg.gestor.domain.model.Projeto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
 
+    @Query("select p from Projeto p where p.nomeprojeto = ?1 ")
+    Optional<Projeto> findByNomeprojeto(String nome);
+
+    @Query("select p from Projeto p where p.idprojeto = ?1")
+    Optional<Projeto> findByidProjeto(long id);
 
 
 }
