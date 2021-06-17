3d7cf6416f3ee93e9c915f74b51d11285267379b
Brayan <brayan.11fera@gmail.com>
2021-06-17 11:34:35 -0300

    Inserção do método para buscar um objeto seção pelo ID

--- src/main/java/net/weg/gestor/domain/repository/SecaoRepository.java
+++ src/main/java/net/weg/gestor/domain/repository/SecaoRepository.java
@@ -3,12 +3,17 @@ package net.weg.gestor.domain.repository;
 import ch.qos.logback.core.pattern.parser.OptionTokenizer;
 import net.weg.gestor.domain.model.Secao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface SecaoRepository extends JpaRepository<Secao, Long> {
+
+    @Query("select p from Secao p where p.idsecao = ?1")
+    Secao findById2(long secaoid);
 
     Optional<Secao> findById(long secaoid);
 
