74fed77cc2bd71326d84a72339038af3cdb7760d
Brayan <brayan.11fera@gmail.com>
2021-06-18 09:42:45 -0300

    Classe que ira receber os dados de cadastro de projetos do front end

--- src/main/java/net/weg/gestor/model/projetoinput/ProjetoInput.java
+++ src/main/java/net/weg/gestor/model/projetoinput/ProjetoInput.java
@@ -1,2 +1,16 @@
-package net.weg.gestor.model.projetoinput;public class ProjetoInput {
+package net.weg.gestor.model.projetoinput;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.weg.gestor.model.GestorModel;
+
+@Getter
+@Setter
+public class ProjetoInput {
+
+    private GestorModel gestor;
+    private String nomeprojeto;
+    private int horasprevistas;
+    private double valorprojeto;
+
 }
