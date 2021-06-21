ec02fa760a09765f95472876f1f720daed07e1c5
Brayan <brayan.11fera@gmail.com>
2021-06-21 15:26:37 -0300

    Classe centro de custo

--- src/main/java/net/weg/gestor/domain/model/CentroDeCusto.java
+++ src/main/java/net/weg/gestor/domain/model/CentroDeCusto.java
@@ -1,2 +1,27 @@
-package net.weg.gestor.domain.model;public class CentroDeCusto {
+package net.weg.gestor.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "ccpagantes")
+public class CentroDeCusto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idcentrodecusto;
+
+    private String nomecentrodecusto;
+
+    @ManyToOne
+    private Projeto projeto;
+
 }
