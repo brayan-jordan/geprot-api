3db59dd70d29fd06dd0c623d27ed4f51ade76752
Brayan <brayan.11fera@gmail.com>
2021-06-22 14:16:51 -0300

    Refatoração na migration com remoção da coluna id_projeto, motivo: terá uma tabela onde estará contido todos os centros de custos dos projetos

--- src/main/resources/db/migration/V0008__createTableCentroDeCustos.sql
+++ src/main/resources/db/migration/V0008__createTableCentroDeCustos.sql
@@ -1,9 +1,5 @@
 CREATE TABLE ccpagantes (
     idcentrodecusto bigint not null auto_increment,
     nomecentrodecusto varchar(100) not null,
-    projeto_idprojeto long not null,
     primary key (idcentrodecusto)
 );
-
-ALTER TABLE ccpagantes ADD CONSTRAINT fk_ccpagantes_projetos
-FOREIGN KEY (projeto_idprojeto) REFERENCES projetos (idprojeto);
