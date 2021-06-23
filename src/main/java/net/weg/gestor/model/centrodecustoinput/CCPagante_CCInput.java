b772e46002d3c9ea9993b0c36ffa85f1777b8a5e
Brayan <brayan.11fera@gmail.com>
2021-06-23 08:57:13 -0300

    Adição das anotações NotNull para evitar que na hora de adicionar um centro de custo o ID chegue null no back end

--- src/main/java/net/weg/gestor/model/centrodecustoinput/CCPagante_CCInput.java
+++ src/main/java/net/weg/gestor/model/centrodecustoinput/CCPagante_CCInput.java
@@ -3,10 +3,13 @@ package net.weg.gestor.model.centrodecustoinput;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class CCPagante_CCInput {
 
+    @NotNull
     private Long codigo;
 
 }
--- src/main/java/net/weg/gestor/model/centrodecustoinput/CCPagante_ProjetoInput.java
+++ src/main/java/net/weg/gestor/model/centrodecustoinput/CCPagante_ProjetoInput.java
@@ -3,10 +3,13 @@ package net.weg.gestor.model.centrodecustoinput;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class CCPagante_ProjetoInput {
 
+    @NotNull
     private Long idprojeto;
 
 }
