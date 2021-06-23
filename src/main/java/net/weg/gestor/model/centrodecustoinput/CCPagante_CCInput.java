e486720d934869a05d6f7ccb1bc58cbc3bde69de
Brayan <brayan.11fera@gmail.com>
2021-06-23 08:47:48 -0300

    Adição das classes que irão ser necessárias para fazer adição dos centros de custos nos projetos

---
+++ src/main/java/net/weg/gestor/model/centrodecustoinput/CCPagante_CCInput.java
@@ -0,0 +1,12 @@
+package net.weg.gestor.model.centrodecustoinput;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CCPagante_CCInput {
+
+    private Long codigo;
+
+}
---
+++ src/main/java/net/weg/gestor/model/centrodecustoinput/CCPagantesInput.java
@@ -0,0 +1,16 @@
+package net.weg.gestor.model.centrodecustoinput;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.weg.gestor.domain.model.Projeto;
+import net.weg.gestor.model.projetoinput.ProjetoInput;
+
+@Getter
+@Setter
+public class CCPagantesInput {
+
+    private CCPagante_ProjetoInput projetoInput;
+
+    private CCPagante_CCInput ccInput;
+
+}
