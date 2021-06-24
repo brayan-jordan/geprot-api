ef2834993da8a77950fbab39d91bdd22be02e3ee
Brayan <brayan.11fera@gmail.com>
2021-06-24 08:00:21 -0300

    Adição da migration para adicionar alguns valores na tabela CentroDeCusto para testar as ações do sistema

---
+++ src/main/resources/db/migration/V0011__insertCentroDeCusto.sql
@@ -0,0 +1,5 @@
+INSERT INTO centrodecusto VALUES (null, "CentroDeCusto1");
+INSERT INTO centrodecusto VALUES (null, "CentroDeCusto2");
+INSERT INTO centrodecusto VALUES (null, "CentroDeCusto3");
+INSERT INTO centrodecusto VALUES (null, "CentroDeCusto4");
+INSERT INTO centrodecusto VALUES (null, "CentroDeCusto5");
