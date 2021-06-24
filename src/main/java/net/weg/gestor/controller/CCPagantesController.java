288b82d2538ce1997c0735e45c831ee830a4068e
Brayan <brayan.11fera@gmail.com>
2021-06-24 07:57:07 -0300

    Adição da classe que irá controlar as rotas das ações do sistema relacionadas a ccPagantes

---
+++ src/main/java/net/weg/gestor/controller/CCPagantesController.java
@@ -0,0 +1,14 @@
+package net.weg.gestor.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/ccpagantes")
+public class CCPagantesController {
+
+
+
+}
--- src/main/java/net/weg/gestor/domain/repository/CCPagantesRepository.java
+++ src/main/java/net/weg/gestor/domain/repository/CCPagantesRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CCPagantesRepository extends JpaRepository<CCPagantes, Long> {
 
-    
+
 
 }
