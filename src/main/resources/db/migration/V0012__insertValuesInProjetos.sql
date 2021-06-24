cd4d15e1525d5e4bbff674908f7a7a93581ff194
Brayan <brayan.11fera@gmail.com>
2021-06-24 08:06:20 -0300

    Migration para inserir alguns valores na tabela de projetos para podermos testar algumas funções da API

---
+++ src/main/resources/db/migration/V0012__insertValuesInProjetos.sql
@@ -0,0 +1,10 @@
+INSERT INTO projetos VALUES
+(null, "Projeto1", 20201224, null, 20, 0, 1000.0, 500.0, 500.0, 67264, "EM_ANDAMENTO");
+INSERT INTO projetos VALUES
+(null, "Projeto2", 20201224, null, 20, 0, 1000.0, 500.0, 500.0, 67264, "EM_ANDAMENTO");
+INSERT INTO projetos VALUES
+(null, "Projeto3", 20201224, null, 20, 0, 1000.0, 500.0, 500.0, 67264, "EM_ANDAMENTO");
+INSERT INTO projetos VALUES
+(null, "Projeto4", 20201224, null, 20, 0, 1000.0, 500.0, 500.0, 67264, "EM_ANDAMENTO");
+INSERT INTO projetos VALUES
+(null, "Projeto5", 20201224, null, 20, 0, 1000.0, 500.0, 500.0, 67264, "EM_ANDAMENTO");
