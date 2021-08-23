3975be206a6288b2136f37d4aab980f02ba26f1e
Brendon <brendon3007gabriel@gmail.com>
2021-08-23 09:33:55 -0300

    Inserção em tabelas

--- src/main/resources/db/migration/V0012__insertValuesInProjetos.sql
+++ src/main/resources/db/migration/V0012__insertValuesInProjetos.sql
@@ -1,6 +1,4 @@
 --INSERT INTO projetos VALUES
---(null, "Projeto1", 20201224, null, 20, 0, 1000.0, 500.0, 500.0, 67264, "EM_ANDAMENTO");
---INSERT INTO projetos VALUES
 --(null, "Projeto2", 20201224, null, 20, 0, 1000.0, 500.0, 500.0, 67264, "EM_ANDAMENTO");
 --INSERT INTO projetos VALUES
 --(null, "Projeto3", 20201224, null, 20, 0, 1000.0, 500.0, 500.0, 67264, "EM_ANDAMENTO");
---
+++ src/main/resources/db/migration/V0021__insertInTables.sql
@@ -0,0 +1,13 @@
+INSERT INTO secoes VALUES
+(null, "CentroWeg"),
+(null, "Recursos Humanos");
+
+INSERT INTO usuarios VALUES (67264,'$2a$10$vAZfFnx9T6IvxlfO9sU4AOQb0qLm/LA10vEY4I2V6CxEkVucuZAHG', '2', 'Brayan', 'brayan@gmail.com');
+
+INSERT INTO role_usuarios VALUES (null,"ROLE_GESTOR", 67264);
+
+INSERT INTO projetos VALUES
+(null, "Projeto1", 20201224, null, 20, 0, 1000.0, 500.0, 500.0, 67264, "EM_ANDAMENTO");
+
+
+
