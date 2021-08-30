d4cbb7006e25233a61c6d7244215f69b39dd8079
Brayan <brayan.11fera@gmail.com>
2021-08-30 11:22:10 -0300

    Criação da classe horas controller e horas service

---
+++ src/main/java/net/weg/gestor/api/controller/HorasController.java
@@ -0,0 +1,9 @@
+package net.weg.gestor.api.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class HorasController {
+}
---
+++ src/main/java/net/weg/gestor/domain/service/HorasService.java
@@ -0,0 +1,9 @@
+package net.weg.gestor.domain.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class HorasService {
+}
