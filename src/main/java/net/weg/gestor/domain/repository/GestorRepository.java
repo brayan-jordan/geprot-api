19a4c6780de4fe755b1d5bde22a6dfa8b25af0ed
Brendon <brendon3007gabriel@gmail.com>
2021-10-08 08:48:13 -0300

    Dale Garaio

--- src/main/java/net/weg/gestor/domain/repository/GestorRepository.java
+++ src/main/java/net/weg/gestor/domain/repository/GestorRepository.java
@@ -2,12 +2,14 @@ package net.weg.gestor.domain.repository;
 
 import net.weg.gestor.domain.entities.Gestor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GestorRepository extends JpaRepository<Gestor, Long> {
 
-//    @Query("SELECT g FROM Gestor g where g.usuarios_id = ?1")
-//    Gestor findByUsuarioId(Long usuarios_id);
+
+    @Query("SELECT g FROM Gestor g where g.usuario.id = ?1")
+    Gestor findByUsuarioId(Long usuarios_id);
 
 }
