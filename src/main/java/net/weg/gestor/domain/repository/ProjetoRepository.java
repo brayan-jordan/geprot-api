8450a6d1a66c26cea1717cde55d76e57be9099b1
brayan <brayan.11fera@gmail.com>
2021-10-28 07:58:43 -0300

    Migration para projetos concluidos + objeto de retorno para dashboards

--- src/main/java/net/weg/gestor/domain/repository/ProjetoRepository.java
+++ src/main/java/net/weg/gestor/domain/repository/ProjetoRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
+
+    @Query("SELECT p From Projeto p where p.dataFinalizacao = ?1")
+    List<Projeto> contarProjetosConcluidosPorDia(LocalDate date);
 
 }
