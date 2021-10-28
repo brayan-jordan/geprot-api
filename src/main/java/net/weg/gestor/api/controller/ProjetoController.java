1107037335a5c7f7508d4fe5ad6056f98cd803b8
brayan <brayan.11fera@gmail.com>
2021-10-28 08:47:01 -0300

    Listar projetos concluidos nos ultimos 7 dias pronto e funcionando

--- src/main/java/net/weg/gestor/api/controller/ProjetoController.java
+++ src/main/java/net/weg/gestor/api/controller/ProjetoController.java
@@ -1,6 +1,7 @@
 package net.weg.gestor.api.controller;
 
 import lombok.AllArgsConstructor;
+import net.weg.gestor.api.model.DashboardConcluidos;
 import net.weg.gestor.api.model.projeto.ProjetoAlocarDTO;
 import net.weg.gestor.api.model.projeto.ProjetoCardDTO;
 import net.weg.gestor.api.model.cadastrarprojetoinput.ProjetoInputDTO;
@@ -146,5 +147,9 @@ public class ProjetoController {
         return projetoService.cadastrarProjeto(projeto);
     }
 
+    @GetMapping("/dashboard/7dias/{secaoId}")
+    public List<DashboardConcluidos> buscarUltimos7dias(@PathVariable Long secaoId) {
+        return projetoService.concluidosUltimos7Dias(secaoId);
+    }
 
 }
