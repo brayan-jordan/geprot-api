9ad366880a37ac4cd929c301f20eb9351c8145fc
brayan <brayan.11fera@gmail.com>
2021-10-29 10:41:08 -0300

    Remoção de classes que poderiam ser reutilizadas porém não serão

--- src/main/java/net/weg/gestor/api/controller/DashboardController.java
+++ src/main/java/net/weg/gestor/api/controller/DashboardController.java
@@ -30,24 +30,5 @@ public class DashboardController {
         return dashboardService.listarProjetos(secaoId);
     }
 
-//    @GetMapping("/concluidos/7dias/{secaoId}")
-//    public List<BasePorMesDashboardDTO> buscar7days(@PathVariable long secaoId) {
-//        return dashboardService.buscar7days(secaoId);
-//    }
-
-//    @GetMapping("/concluidos/ultimoMes/{secaoId}")
-//    public List<BasePorMesDashboardDTO> buscarUltimoMes(@PathVariable long secaoId) {
-//        return dashboardService.buscarUltimoMes(secaoId);
-//    }
-
-//    @GetMapping("/concluidos/ultimos6meses/{secaoId}")
-//    public List<BasePorMesDashboardDTO> buscarUltimos6meses(@PathVariable long secaoId) {
-//        return dashboardService.buscarUltimos6meses(secaoId);
-//    }
-
-//    @GetMapping("/concluidos/ultimoano/{secaoId}")
-//    public List<BasePorMesDashboardDTO> buscarUltimoAno(@PathVariable long secaoId) {
-//        return dashboardService.buscarUltimoAno(secaoId);
-//    }
 
 }
